64a41049357c5aeb00a8f92cffb2142c811ef471
Mitch Mele <mitchmele@gmail.com>
2018-03-18 09:33:03 -0500

    Add static pages. Setup root route for application.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  ##static home page
+  root 'static_pages#home'
+
+  ##Static Help Page displays in-App README 
+  get 'static_pages/help'
+
+  ##More about me:
+  get 'static_pages/about'
+
+
+
+  
+
 end
