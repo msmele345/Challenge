74915e303126577bbb6cdac7c61137a32679f296
Mitch Mele <mitchmele@gmail.com>
2018-03-20 13:39:17 -0500

    Add algoritms in helpers to help clean up controller logic.

--- app/controllers/damage_reports_controller.rb
+++ app/controllers/damage_reports_controller.rb
@@ -9,10 +9,6 @@ class DamageReportsController < ApplicationController
       @damage_report = DamageReport.new
     end 
 
-    ##Identify and Save weapon from form params 
-    ##Identify Character from character params 
-    ##Call grab_attack_names from helpers to create array of all current attack names 
-    ##Iterate over 
 
     def create
       @weapon = Weapon.new(weapon_params)      
@@ -24,24 +20,48 @@ class DamageReportsController < ApplicationController
       end 
 
       @character = Character.find_by(:name => params[:character_name])
+      @damage_range = parse_dmg_range(params[:DMG_range])
+      @character_input = character_input(@weapon.weapon_attr_modifier, @character.level)
       @attacks = grab_attack_names 
+      @element_check = false
+
 
       @attacks.each do |attack_name| ## this wont work if attacks are deleted (indexes incorrect)
         attack = Attack.find_by(:attack_name => attack_name)
-        # p"**********"
-        # p attack_index
+        attack_dmg = attack.DMG_impact
+        # p "********"
+        # ep @weapon.element_impact
+        # ep attack.element
+        ep attack
+        if attack.element === @weapon.element_impact
+          @element_check = true  
+        end 
+
+        p calculate_min_damage(@damage_range[:min_damage], @character_input, attack_dmg, @weapon.element_boost, @element_check )
+
         # damage_report = DamageReport.create!(:weapon_id => @weapon.id, :attack_id => attack.id, :min_damage => helper method, :max_damage => helper_method, :damage_per_second)
       end 
 
+      # def calculate_min_damage(weapon_min, character_input, attack_dmg, element_boost, element_impact = nil)
+      #   element_dmg = element_dmg / 10
+      #   base_amount =  min_damage * character_input * attack_dmg
+    
+      #   if element_impact
+      #     boost_amt = base_amount * element_dmg
+      #     return base_amount + boost_amt
+      #   else 
+      #     return base_amount
+      #  end 
+      # end 
+
       redirect_to damage_reports_path
       ##TODO 
-      ##EACH ATTACK IS ITS OWN DAMAGE REPORT
+      ##FIX element boost 
+      ##Create max algo 
+      ##work on displays 
       ##Display all Damage reports on index page!
       ##display attacks as part of table 
-      ##Show APS legend on the left side bar of damage new 
-
       ##OR iterate through attacks. Setup if statements for name chaecks to apply defaults 
-      ##start making helpers for algos
     end 
 
     def destroy 
--- app/helpers/damage_reports_helper.rb
+++ app/helpers/damage_reports_helper.rb
@@ -1,7 +1,9 @@
 module DamageReportsHelper 
 
+  ##Method to parse the damage range of the weapon. Displayed as a hash
   def parse_dmg_range(range)
-    range.split
+    number_array = range.split("-").map {|string_num| string_num.to_i }
+    hash_display = {:min_damage => number_array[0], :max_damage => number_array[1]} 
   end 
 
   ##Helper to display all active characters on damage report create view  
@@ -14,6 +16,31 @@ module DamageReportsHelper
 
   def grab_attack_names
     attacks = Attack.all.pluck(:attack_name)
+  end 
+
+  def character_input(weapon_attr_modifier, level)
+    total_character_damage = ((level * 10 ) + weapon_attr_modifier).to_f
+
+  end 
+
+  def calculate_min_damage(weapon_min, character_input, attack_dmg, element_boost, element_impact = nil)
+    # p weapon_min
+    # p character_input
+    # p attack_dmg
+    # p element_boost
+    ep element_impact
+    element_dmg = element_boost / 100
+    base_amount =  weapon_min * (character_input / 100) * (attack_dmg / 100)
+
+    if element_impact 
+      boost_amt = base_amount * element_dmg
+      return base_amount + boost_amt
+    end 
+    base_amount < 1 ? base_amount * 100 : base_amount
+  end 
+
+  def calculate_max_damage(max_damage, character_level, attack_dmg)
+    
   end 
 
 
