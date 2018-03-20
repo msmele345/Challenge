e8df1a3315e084c6f5577bc5b952b258ee9a0339
Mitch Mele <mitchmele@gmail.com>
2018-03-19 21:33:30 -0500

    Update create controller logic to incorporate all three objects.

--- app/controllers/damage_reports_controller.rb
+++ app/controllers/damage_reports_controller.rb
@@ -10,23 +10,22 @@ class DamageReportsController < ApplicationController
     end 
 
     def create
+      ##NEED CONTROLL FLOW logic for weapons not saving to prevent dupes 
       @weapon = Weapon.create!(weapon_params)
       @character = Character.find_by(:name => params[:character_name])
-      @attacks = grab_attack_names
+      @attacks = grab_attack_names ## zip this with a plucked index value array
 
-      ##damage report has many attacks - one to many. Might need to fix 
-      # @damage_report = DamageReport.new(:weapon_id => @weapon.id, :min_damage => helper method, :max_damage => helper_method, :damage_per_second)
+      @attacks.each_with_index do |attack_name, index| ## this wont work if attacks are deleted (indexes incorrect)
+        @damage_report = DamageReport.create!(:weapon_id => @weapon.id, :attack_id => index :min_damage => helper method, :max_damage => helper_method, :damage_per_second)
 
-
-      redirect_to new_damage_report_path
+      redirect_to damage_reports_path
       ##TODO 
-      ##Figure out Attacks association 
+      ##EACH ATTACK IS ITS OWN DAMAGE REPORT
+      ##Display all Damage reports on index page!
       ##display attacks as part of table 
       ##iterate of both with each with index? 
+      ##OR iterate through attacks. Setup if statements for name chaecks to apply defaults 
       ##start making helpers for algos
-
-      # @damage_report = DamageReport.new(params[:])
-
     end 
 
     def destroy 
